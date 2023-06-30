e7f7917ac71e041b95ae46b6300ad147a2d13e1d
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-30 11:47:10 +0800

    making input component reusable and dynamic, setting up inputs value and handler in the auth.

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,13 +1,26 @@
-const Input = () => {
+import React from "react";
+
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
     <div className="relative">
       <input
-        id="email"
+        id={id}
+        onChange={onChange}
+        value={value}
+        type={type}
         className="block rounded-md pt-6 px-6 pb-1 text-md text-white bg-neutral-700 appearance-none focus:outline-none focus: ring-0 peer"
         placeholder=" "
       />
       <label
-        htmlFor="email"
+        htmlFor={id}
         className="
         absolute
         text-md 
@@ -24,7 +37,7 @@ const Input = () => {
         peer-placeholder-shown:translate-y-0 
         peer-focus:scale-75 
         peer-focus:-translate-y-3">
-        Email
+        {label}
       </label>
     </div>
   );
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,15 @@
+import React, { use, useState } from "react";
 import Input from "@/components/Input";
 
 const Auth = () => {
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  console.log("email", input);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover bg center">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -11,7 +20,32 @@ const Auth = () => {
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
             <div className="flex flex-col gap-4 text-white">
-              <Input />
+              <Input
+                id="name"
+                label="Username"
+                value={input.username}
+                onChange={(e: { target: { value: any } }) => {
+                  return setInput({ ...input, username: e.target.value });
+                }}
+              />
+              <Input
+                type="email"
+                id="email"
+                label="Email"
+                value={input.email}
+                onChange={(e: { target: { value: any } }) => {
+                  return setInput({ ...input, email: e.target.value });
+                }}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={input.password}
+                onChange={(e: { target: { value: any } }) => {
+                  return setInput({ ...input, password: e.target.value });
+                }}
+              />
             </div>
           </div>
         </div>
