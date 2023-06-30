cb81e362a421fc718fecadc2c734c6254941ac1e
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-30 11:18:37 +0800

    setting up form in auth and reusable input and label component.

---
+++ components/Input.tsx
@@ -0,0 +1,33 @@
+const Input = () => {
+  return (
+    <div className="relative">
+      <input
+        id="email"
+        className="block rounded-md pt-6 px-6 pb-1 text-md text-white bg-neutral-700 appearance-none focus:outline-none focus: ring-0 peer"
+        placeholder=" "
+      />
+      <label
+        htmlFor="email"
+        className="
+        absolute
+        text-md 
+        text-zinc-400 
+        duration-150 
+        transform 
+        -translate-y-3 
+        scale-75 
+        top-4 
+        z-10 
+        origin-[0] 
+        left-6 
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75 
+        peer-focus:-translate-y-3">
+        Email
+      </label>
+    </div>
+  );
+};
+
+export default Input;
