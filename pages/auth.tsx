5f7159612e2defb5dcc8e43b824238cd4a1f2e6b
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-30 10:30:40 +0800

    setting up packages, setup auth component and styles. importing logo to images.

---
+++ pages/auth.tsx
@@ -0,0 +1,13 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover bg center">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5 text-m">
+          <img src="images/logo.png" alt="Logo" className="h-12" />
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
